1a60fafd44a1d6948a8998f22e23bc1f4a9a8c09
tanishkagupta-19 <tanishhkkaaa@gmail.com>
2025-11-25 11:28:05 +0530

    feat: adding scraping from web feature

--- crawler/scraper.py
+++ crawler/scraper.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 HEADERS={
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML,like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML,like Gecko) Chrome/91.0.4472.124 Safari/537.36'
 }
 def scrape_times_of_india():
     url="https://timesofindia.indiatimes.com"
